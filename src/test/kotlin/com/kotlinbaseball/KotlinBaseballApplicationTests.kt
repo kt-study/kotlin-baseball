3b906c1d043096e00d1e47dbcb8a04090ca6649c
xrabcde <xrabcde@gmail.com>
2022-02-09 22:44:18 +0900

    feat: 숫자야구게임 환경세팅

---
+++ src/test/kotlin/com/kotlinbaseball/KotlinBaseballApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinbaseball
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBaseballApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
