9faffe6112248c32a789da527f049f2331907f58
Hyeon9mak <jinha3507@gmail.com>
2022-02-27 15:58:39 +0900

    feat: 사용자로부터 입력받은 값에 따라 3개 Baseball 발급 구현

--- src/main/kotlin/com/kotlinbaseball/domain/Baseballs.kt
+++ src/main/kotlin/com/kotlinbaseball/domain/Baseballs.kt
@@ -2,20 +2,59 @@ package com.kotlinbaseball.domain
 
 import kotlin.random.Random
 
-private const val BALL_COUNT: Int = 3
-
-class Baseballs(
-    val values: Set<Baseball> = initValues()
+class Baseballs private constructor(
+    val values: Set<Baseball>
 ) {
-}
 
-fun initValues(): Set<Baseball> {
-    val values = mutableSetOf<Int>()
+    companion object {
+        const val BALL_COUNT: Int = 3
 
-    while (values.size < BALL_COUNT) {
-        values.add(Random.nextInt(Baseball.MIN_NUMBER, Baseball.MAX_NUMBER))
+        fun init(): Baseballs {
+            val values = mutableSetOf<Int>()
+
+            while (values.size < BALL_COUNT) {
+                values.add(Random.nextInt(Baseball.MIN_NUMBER, Baseball.MAX_NUMBER))
+            }
+
+            return Baseballs(
+                values.map { Baseball(it) }
+                    .toSet()
+            )
+        }
+
+        fun from(string: String): Baseballs {
+            validateLength(string)
+            validateDigits(string)
+
+            val values = setOf(string[0], string[1], string[2])
+                .map { Baseball(it.digitToInt()) }
+                .toSet()
+
+            validateDuplicate(values)
+
+            return Baseballs(values)
+        }
+
+        private fun validateDigits(string: String) {
+            string.toCharArray().forEach { validateDigit(it) }
+        }
+
+        private fun validateDigit(character: Char) {
+            if (!character.isDigit()) {
+                throw IllegalArgumentException("입력된 문자 $character 는 정수가 아닙니다.")
+            }
+        }
+
+        private fun validateLength(string: String) {
+            if (string.length != BALL_COUNT) {
+                throw IllegalArgumentException("입력된 문자열의 길이 ${string.length} 가 $BALL_COUNT 와 다릅니다.")
+            }
+        }
+
+        private fun validateDuplicate(values: Set<Baseball>) {
+            if (values.size != BALL_COUNT) {
+                throw IllegalArgumentException("중복된 숫자가 입력되었습니다.")
+            }
+        }
     }
-
-    return values.map { Baseball(it) }
-        .toSet()
 }
--- src/test/kotlin/com/kotlinbaseball/domain/BaseballsTest.kt
+++ src/test/kotlin/com/kotlinbaseball/domain/BaseballsTest.kt
@@ -1,15 +1,63 @@
 package com.kotlinbaseball.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
 
 internal class BaseballsTest : StringSpec({
 
-    "임의의 서로 다룬 Baseball 3개를 만든 후 저장한다" {
+    "임의의 서로 다른 Baseball 3개를 만든 후 저장한다." {
         // when
-        val baseballs = Baseballs()
+        val baseballs = Baseballs.init()
 
         // then
         baseballs.values shouldHaveSize 3
+    }
+
+    "입력된 값에 따라 Baseball 3개를 만든 후 저장한다." {
+        // when
+        val baseBalls = Baseballs.from("123")
+
+        // then
+        baseBalls.values shouldHaveSize 3
+        baseBalls.values.shouldContainExactly(Baseball(1), Baseball(2), Baseball(3))
+    }
+
+    "입력된 값에 정수가 아닌 값이 포함된 경우 예외가 발생한다." {
+        // given
+        val errorCondition = "#"
+
+        // when
+        val exception = shouldThrow<IllegalArgumentException> { Baseballs.from("12$errorCondition") }
+
+        // then
+        exception.message shouldBe "입력된 문자 $errorCondition 는 정수가 아닙니다."
+    }
+
+    "입력된 값의 길이가 3이 아니면 예외가 발생한다." {
+        // given
+        val tooShortCondition = "12"
+        val tooLongCondition = "1234"
+
+        // when
+        val shortException = shouldThrow<IllegalArgumentException> { Baseballs.from(tooShortCondition) }
+        val longException = shouldThrow<IllegalArgumentException> { Baseballs.from(tooLongCondition) }
+
+        // then
+        shortException.message shouldBe "입력된 문자열의 길이 ${tooShortCondition.length} 가 ${Baseballs.BALL_COUNT} 와 다릅니다."
+        longException.message shouldBe "입력된 문자열의 길이 ${tooLongCondition.length} 가 ${Baseballs.BALL_COUNT} 와 다릅니다."
+    }
+
+    "입력된 값에 중복된 정수가 포함된 경우 예외가 발생한다." {
+        // given
+        val duplicateCondition = "111"
+
+        // when
+        val duplicateException = shouldThrow<IllegalArgumentException> { Baseballs.from(duplicateCondition) }
+
+        // then
+        duplicateException.message shouldBe "중복된 숫자가 입력되었습니다."
     }
 })
